4f3293b2e41e7d8717f66a57f7adf03c188c32fc
omarheeh <pazwut-1bidwY-xobjyt>
2024-01-21 11:05:31 +0200

    -added Development and Production flavors for android -changed android app name for each flavor -added launch.json for flavors config -added two main files for each flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,9 +2,11 @@ import 'package:doctormobile/core/di/dependency_injection.dart';
 import 'package:doctormobile/core/routing/app_router.dart';
 import 'package:doctormobile/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   initDependencyInjection();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,8 +10,6 @@ import 'package:doctormobile/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:doctormobile/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
